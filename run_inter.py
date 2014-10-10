2479ed3e7932538e9fcb45a58387c3543458a4a8
393099545@qq.com <cmiao@apple.(none)>
2014-10-10 10:19:48 +0800

    hehe

--- run_inter.py
+++ run_inter.py
@@ -6,10 +6,10 @@ from subprocess import call
 def runInterProscan(seqdir):
     f0 = open('run_commands.sh', 'w')
     proteins = [os.path.abspath('%s/'%seqdir+j) for j in os.listdir(seqdir)]
-    print proteins
     for i in proteins:
         opfile = 'myresults/'+os.path.basename(i).split('.')[0]
         cmd = './interproscan.sh -i %s -b %s -goterms -pa\n'%(i, opfile)
+        print cmd
         f0.write(cmd)
     f0.close()
     call('chmod 777 run_commands.sh', shell=True)
